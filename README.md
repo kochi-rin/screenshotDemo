bd9a20d8ade8f0c9126a1a10779678e3974517f8
kochi-rin <kochi.rin@zozo.com>
2020-06-18 16:28:53 +0900

    add bash

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Run test
 ```
-xcodebuild -scheme screeshot -project ./screeshot.xcodeproj -derivedDataPath './ScreenshotOutput/Logs/Test/' -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3' build test
+xcodebuild -scheme screeshot -project ./screeshot.xcodeproj -derivedDataPath './ScreenshotOutput' -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3' build test
 ```
 
 ### Export png
---
+++ takeScreenshots.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# The Xcode project to create screenshots for
+projectName="./screeshot.xcodeproj"
+
+# The scheme to run tests for
+schemeName="screeshot"
+
+
+# All the simulators we want to screenshot
+# Copy/Paste new names from Xcode's
+# "Devices and Simulators" window
+# or from `xcrun simctl list`.
+simulators=(
+    "iPhone 11 Pro"
+)
+
+# All the languages we want to screenshot (ISO 3166-1 codes)
+languages=(
+    "en"
+)
+
+# All the appearances we want to screenshot
+# (options are "light" and "dark")
+appearances=(
+    "light"
+)
+
+#Save the test result
+testResultFolder="./ScreenshotOutput/Test"
+
+# Save final screenshots into this folder (it will be created)
+targetFolder="./ScreenshotOutput/Screenshots"
+
+
+## No need to edit anything beyond this point
+
+
+for simulator in "${simulators[@]}"
+do
+    for language in "${languages[@]}"
+    do
+        for appearance in "${appearances[@]}"
+        do
+            rm -rf $testResultFolder
+            echo "📲  Building and Running for $simulator in $language"
+
+            # Boot up the new simulator and set it to 
+            # the correct appearance
+            xcrun simctl boot "$simulator"
+            xcrun simctl ui "$simulator" appearance $appearance
+
+            # Build and Test
+            xcodebuild -testLanguage $language -scheme $schemeName -project $projectName -derivedDataPath $testResultFolder -destination "platform=iOS Simulator,name=$simulator" build test
+            echo "🖼  Collecting Results..."
+            mkdir -p "$targetFolder/$simulator/$language/$appearance"
+            find "$testResultFolder/Logs/Test" -maxdepth 1 -type d -exec xcparse screenshots {} "$targetFolder/$simulator/$language/$appearance" \;
+        done
+    done
+
+    echo "✅  Done"
+done
+
